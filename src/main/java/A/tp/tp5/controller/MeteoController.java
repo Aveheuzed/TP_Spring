e76a4cfba335a515b0ad0bf7a9e95b1528cb8527
Aveheuzed <a.masson555@ntymail.com>
2020-12-21 15:09:37 +0100

    Consumed the Weather RESY API to display weather forecast

--- src/main/java/A/tp/tp5/controller/MeteoController.java
+++ src/main/java/A/tp/tp5/controller/MeteoController.java
@@ -6,17 +6,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
-import A.tp.tp5.model.AdresseEtalab;
+import A.tp.tp5.model.AdresseJSON.AdresseEtalab;
+import A.tp.tp5.model.MeteoJSON.WeatherResult;
 
 @Controller
 public class MeteoController {
 	
+	private final static String APIKey = "058ce7f157a747fa866fe6e6004808bc";
+	
 	@PostMapping("/meteo")
 	public String servePage(@RequestParam(name="query", required=true) String adresse, Model model) {
 		String url = String.format("https://api-adresse.data.gouv.fr/search/?q=\"%s\"&limit=1", adresse);
-		double response[] = new RestTemplate().getForObject(url, AdresseEtalab.class).features[0].geometry.coordinates;
-		model.addAttribute("gps_x", response[0]);
-		model.addAttribute("gps_y", response[1]);
+		double reponse_gps[] = new RestTemplate().getForObject(url, AdresseEtalab.class).features[0].geometry.coordinates;
+		
+		url = String.format("http://api.openweathermap.org/data/2.5/weather?lon=%f&lat=%f&appid=%s&lang=fr&units=metric", reponse_gps[0], reponse_gps[1], MeteoController.APIKey );
+		WeatherResult reponse_meteo = new RestTemplate().getForObject(url, WeatherResult.class);
+		
+		String prevision = reponse_meteo.weather[0].description;
+		double temperature = reponse_meteo.main.temp; 
+		
+		model.addAttribute("gps_x", reponse_gps[0]);
+		model.addAttribute("gps_y", reponse_gps[1]);
+		model.addAttribute("prevision", prevision);
+		model.addAttribute("temperature", temperature);
 		model.addAttribute("query", adresse);
 		return "meteo";
 	}
